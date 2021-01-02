64966e6b9f2ab913ae1a866eca505d501dc2e904
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-01-02 01:06:45 -0800

    Render TopStoriesContainer in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
+/**NY Times Lite Application
+ * 
+ * App -> TopNewsContainer
+ */
 function App() {
+
   return (
     <div className="App">
-      
+      <h1>NY Times Lite</h1>
+
+      <TopNewsContainer />
     </div>
   );
 }
